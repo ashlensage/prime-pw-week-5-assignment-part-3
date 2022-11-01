1ee8b92d14b6059a22180e932b745eac6683aafa
Ashlen Goff <ashlensage@icloud.com>
2022-11-01 17:09:58 -0500

    finished week 5 part 3 assignmnet

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,42 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
+const collection = [];
+function addToCollection(title, artist, yearPublished) {
+    const newItem = {
+        title, 
+        artist, 
+        yearPublished
+    };
+    collection.push(newItem);
+    return newItem;
+}
+
+console.log('item added to collection:', addToCollection('Drivers license', 'Olivia Rodrigo', 2021));
+console.log('item added to collection:', addToCollection('Coaster', 'Khalid', 2017));
+console.log('item added to collection:', addToCollection('The Call', 'Regina Spektor', 2009));
+console.log('item added to collection:', addToCollection('Hold Me While You Wait', 'Lewis Capaldi', 2019));
+console.log('item added to collection:', addToCollection('Someone You Loved', 'Lewis Capaldi', 2019));
+console.log('item added to collection:', addToCollection('Boys Like You', 'Anna Clendening', 2017));
+
+console.log('collection:', collection);
+
+function showCollection(array) {
+    console.log('number of items in array:', array.length);
+    for (let i = 0; i < array.length; i++) {
+        console.log(`"${array[i].title}" by ${array[i].artist}, published in ${array[i].yearPublished}`);
+    }
+} 
+showCollection(collection);
+
+function findByArtist(artist) {
+    const foundMatches = [];
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i].artist === artist) {
+            foundMatches.push(collection[i]);
+        }
+    }
+    return foundMatches;
+}
+
+console.log('find a single artist:', findByArtist('Khalid'));
+console.log('find artist multiple matches:', findByArtist('Lewis Capaldi'));
+console.log('unable to find artist:', findByArtist('Selena Gomez'));
